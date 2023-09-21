aa2970b42c599e2587abee25d3d9240949fe7dd1
sgillot <sgillot@beapi.fr>
2023-09-21 16:23:54 +0200

    issue/65827 : add the compat flag for hpos

--- shoppingfeed-custom-fields.php
+++ shoppingfeed-custom-fields.php
@@ -37,3 +37,9 @@ function init() {
 }
 
 \add_action( 'plugins_loaded', __NAMESPACE__ . '\\init' );
+
+\add_action( 'before_woocommerce_init', function() {
+	if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+	}
+} );
